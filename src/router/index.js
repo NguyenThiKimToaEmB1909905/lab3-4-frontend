4d5bb795f6db85c135e874ffbdac97634748aefc
NguyenThiKimToaEm/B1909905 <emb1909905@student.ctu.edu.vn>
2022-10-12 21:31:05 +0700

    cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -7,11 +7,12 @@ const routes = [
         name: "contactbook",
         component: ContactBook,
     },
-//     {
-//         path: "/:pathMatch(.*)*",
-//         name: "notfound",
-//         component: () => import("@/views/NotFound.vue"),
-//     },
+    {
+        
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+    },
 
 //     {
 //         path: "/contacts/:id",
